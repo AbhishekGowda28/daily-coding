91a1ae360e7acf1c8b5cae70c1bc205de838d4b5
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-02-18 21:33:40 +0530

    Added perspective

--- wireframe-renderer/main.js
+++ wireframe-renderer/main.js
@@ -5,6 +5,7 @@ import { square } from './models.js';
 function setUpCanvas() {
     const canvas = document.querySelector("canvas");
     const context = canvas.getContext("2d");
+    context.strokeStyle = "#fff";
     return context;
 }
 
@@ -35,7 +36,55 @@ function toMesh(shape) {
     return shape.map(toPolygon);
 }
 
-const canvas = setUpCanvas();
-const mesh = toMesh(square);
+/**
+ * @param {{ x: number; y: number; z: number; }[]} shape
+ * @param {CanvasRenderingContext2D} context
+ */
+function drawPolygon(shape, context) {
+    shape.forEach(point => {
+        offsetToCenter(point, context.canvas);
+    });
+    context.beginPath();
 
-window.console.log("canvas context", canvas, square, mesh);
+    const first = shape[0];
+    context.moveTo(first.x, first.y);
+    shape.forEach(point => {
+        context.lineTo(point.x, point.y);
+    });
+    context.lineTo(first.x, first.y);
+    context.stroke();
+
+}
+
+/**
+ * @param {{ x: number; y: number; z: number; }} point
+ * @param {HTMLCanvasElement} canvas
+ */
+function offsetToCenter(point, canvas) {
+    point.x += canvas.width / 2;
+    point.y += canvas.height / 2;
+}
+
+
+/**
+ * @param {{ x: number; y: number; z: number; }} point
+ * @param {number} distance
+ */
+function persepective(point, distance) {
+    const focal = point.z + distance;
+    point.x /= focal;
+    point.z /= focal;
+}
+
+
+const mesh = toMesh(square);
+const canvas = setUpCanvas();
+
+mesh.forEach(shape => {
+    shape.forEach(point => {
+        window.console.log("before", point);
+        persepective(point, 50);
+        window.console.log("after", point);
+    });
+    drawPolygon(shape, canvas);
+});
