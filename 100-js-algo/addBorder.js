cf64923e708720b70ee3ccc64e70ff8f093187f3
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-02-27 22:21:01 +0530

    Refactored Add-border problem

--- 100-js-algo/addBorder.js
+++ 100-js-algo/addBorder.js
@@ -1,18 +1,17 @@
 function addBorder(picture) {
     const borderLength = picture.length > 0 ? picture[0].length + 2 : 2;
-    const borderedPicture = [];
     const border = "*".repeat(borderLength);
-
-    borderedPicture.push(border);
+    const borderedPicture = [border];
 
     picture.forEach(str => {
         borderedPicture.push(`*${str}*`);
     });
 
     borderedPicture.push(border);
-    
+
     return borderedPicture;
 }
 
 addBorder(["abc", "def"]);
+addBorder(['']);
 addBorder([]);
