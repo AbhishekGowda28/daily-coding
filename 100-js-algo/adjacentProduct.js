b0cb05f3911178bbef400e738510f46d4cb0e3ec
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-03-01 23:21:46 +0530

    Optimized solution for adjacent product

--- 100-js-algo/adjacentProduct.js
+++ 100-js-algo/adjacentProduct.js
@@ -1,13 +1,19 @@
 function adjacentProduct(matrix) {
+    const initalValue = matrix.length > 1 ? matrix[0] * matrix[1] : matrix.length === 1 ? matrix[0] : 0;
     return matrix.reduce((totalProd, element, index) => {
-        const product = element * matrix[index + 1];
-        if (product > totalProd) {
-            return product;
+        if (index < matrix.length - 1) {
+            const product = element * matrix[index + 1];
+            if (product > totalProd) {
+                return product;
+            }
         }
         return totalProd;
-    }, 1);
+    }, initalValue);
 }
 
-adjacentProduct([]);
-adjacentProduct([1, 2, 3, 4]);
-adjacentProduct([3, 6, -2, -5, 7, 3]);
+console.log(adjacentProduct([]))
+console.log(adjacentProduct([1]));
+console.log(adjacentProduct([1, -1, 2]))
+console.log(adjacentProduct([1, -2]));
+console.log(adjacentProduct([1, 2, 3, 4]));
+console.log(adjacentProduct([3, 6, -2, -5, 7, 3]));
