65c21d8e292a38b97db9bf40a1f6c0d81f7048a7
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-03-07 23:25:03 +0530

    Added method to check the increasing sequence

--- 100-js-algo/increasingSequence.js
+++ 100-js-algo/increasingSequence.js
@@ -1,7 +1,27 @@
-function increasingSequence(){
+function increasingSequence() {
   console.log("increasingSequence");
+  console.log(checkIncreasingSequence([1, 2, 3, 4]));
+  console.log(checkIncreasingSequence([1, 2, 5, 4]));
+  console.log(checkIncreasingSequence([]));
+  console.log(checkIncreasingSequence([1]));
+  console.log(checkIncreasingSequence([2, 1]));
 }
 
-module.exports = {
-  increasingSequence,
-};
+function checkIncreasingSequence(array) {
+  let increaseingOrder = true;
+  let previous = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (previous > array[i]) {
+      increaseingOrder = false;
+    }
+    previous = array[i];
+  }
+
+  return increaseingOrder;
+}
+
+// module.exports = {
+//   increasingSequence,
+// };
+
+increasingSequence();
