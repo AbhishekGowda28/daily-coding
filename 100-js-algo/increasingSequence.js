eaf58732cf6db0f35cf57d5b5de9ce92a8f123fd
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-03-08 17:54:58 +0530

    Solved almost increasing sequence problem

--- 100-js-algo/increasingSequence.js
+++ 100-js-algo/increasingSequence.js
@@ -1,5 +1,13 @@
-function increasingSequence() {
-  // console.log("increasingSequence");
+function increasingSequence(input) {
+  const sequence = input.map((element, index) => {
+
+    const clone = input.slice();
+    clone.splice(index, 1);
+
+    return checkIncreasingSequence(clone);
+  });
+
+  return !sequence.includes(false);
 }
 
 function checkIncreasingSequence(array) {
@@ -7,7 +15,7 @@ function checkIncreasingSequence(array) {
   let previous = array[0];
 
   for (let i = 1; i < array.length; i++) {
-    if (previous > array[i]) {
+    if (previous >= array[i]) {
       increaseingOrder = false;
     }
     previous = array[i];
@@ -21,4 +29,5 @@ module.exports = {
   checkIncreasingSequence
 };
 
-increasingSequence();
+console.log(increasingSequence([1, 2, 3, 2]));
+console.log(increasingSequence([1, 2, 3]));
