74703f90f137c3c2ce2acdf78fb3af903ecee0dd
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-03-12 23:49:10 +0530

    Random choice

---
+++ 100-js-algo/randomChoice.js
@@ -0,0 +1,7 @@
+// @ts-check
+
+function randomChoice(n umberOfChoices){
+    for(let choiceNumber = 1; choiceNumber <= numberOfChoices; choiceNumber++){
+    console.log("choice count", choiceNumber);
+}
+}     
