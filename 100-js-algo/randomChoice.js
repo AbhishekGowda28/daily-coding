d348f2f832517c4bdee1cedc00a9eeb6d5713f14
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-03-13 22:30:43 +0530

    Added readline and process

--- 100-js-algo/randomChoice.js
+++ 100-js-algo/randomChoice.js
@@ -1,7 +1,20 @@
 // @ts-check
 
+const readLine = require("readline");
+const process = require("process");
+
+const readLineInterface = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 function randomChoice(numberOfChoices) {
     for (let choiceNumber = 1; choiceNumber <= numberOfChoices; choiceNumber++) {
-        console.log("choice count", choiceNumber);
+        readLineInterface.question(`Enter choice ${choiceNumber}`, (choice) => {
+            console.log("choice", choice);
+        });
     }
+    readLineInterface.close();
 }
+
+randomChoice(5);
