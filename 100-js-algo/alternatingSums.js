84449332bd0fabb8039aaacd1543180192f5280b
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-03-18 22:18:25 +0530

    Solved alternating sum problem

--- 100-js-algo/alternatingSums.js
+++ 100-js-algo/alternatingSums.js
@@ -1,5 +1,13 @@
 function alternatingSums(weights) {
-    return weights;
+    let oddSum = 0, evenSum = 0;
+    weights.forEach((weight, index) => {
+        if (index % 2 === 0) {
+            oddSum += weight;
+        } else {
+            evenSum += weight;
+        }
+    });
+    return [oddSum, evenSum];
 }
 
 module.exports = { alternatingSums };
