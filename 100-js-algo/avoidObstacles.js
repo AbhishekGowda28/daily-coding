b4fc285857673f29980232ab0b84a2f140b1d7db
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-03-31 20:24:10 +0530

    Solved border cases

--- 100-js-algo/avoidObstacles.js
+++ 100-js-algo/avoidObstacles.js
@@ -1,5 +1,15 @@
 function avoidObstacles(inputArray) {
+    if (inputArray.length === 0) {
+        return 0;
+    }
+    inputArray.sort((a, b) => {
+        return (a - b);
+    });
+    if (inputArray[inputArray.length - 1] < 0) {
+        return 1;
+    }
+
     return inputArray.length;
 }
 
-export.modules = { avoidObstacles };
+module.exports = { avoidObstacles };
