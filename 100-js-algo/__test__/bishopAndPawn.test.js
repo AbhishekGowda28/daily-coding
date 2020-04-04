0b744d048702dea9e1eae808ad0c02461027ce59
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-04-04 21:39:59 +0530

    Added bishop and pawn unit test

---
+++ 100-js-algo/__test__/bishopAndPawn.test.js
@@ -0,0 +1,17 @@
+const expect = require("expect");
+const { bishopAndPawn } = require("../bishopAndPawn");
+
+describe("Bishop and Pawn", () => {
+    test("Bishop capturing the pawn", () => {
+        const bishopLocation = "a1";
+        const pawnLocation = "c3";
+        const actual = bishopAndPawn(bishopAndPawn, pawnLocation);
+        expect(actual).toBeTruthy();
+    });
+    test("Bishop not capturing the pawn", () => {
+        const bishopLocation = "h1";
+        const pawnLocation = "h3";
+        const actual = bishopAndPawn(bishopAndPawn, pawnLocation);
+        expect(actual).toBeFalsy();
+    });
+});
---
+++ 100-js-algo/bishopAndPawn.js
@@ -0,0 +1,5 @@
+function bishopAndPawn(bishop, pawn) {
+
+}
+
+module.exports = { bishopAndPawn };
