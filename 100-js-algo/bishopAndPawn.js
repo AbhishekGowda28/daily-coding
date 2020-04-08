9aacb55d9c5a509b159cae5e136312dbaaccb2ed
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-04-08 21:54:03 +0530

    modified chessboard

--- 100-js-algo/bishopAndPawn.js
+++ 100-js-algo/bishopAndPawn.js
@@ -1,12 +1,12 @@
 const chessBoard = [
-    ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
-    ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
-    ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
-    ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
-    ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
-    ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
-    ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
     ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
+    ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
+    ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
+    ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
+    ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
+    ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
+    ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+    ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
 ];
 
 function topRightMovement(bishop, pawn) {
@@ -29,10 +29,10 @@ function topLeftMovement(bishop, pawn) {
     const location = findPawn(bishop);
     let num = location.num;
     let alpha = location.alpha;
+    console.log("topLeftMovement", num, alpha, chessBoard[num][alpha], pawn);
     while ((num < 7 && num > 0) && (alpha < 7 && alpha > 0)) {
-        num--;
+        num++;
         alpha--;
-        console.log("topLeftMovement", num, alpha, chessBoard[num][alpha], pawn);
         if (chessBoard[num][alpha] === pawn) {
             return true;
         }
@@ -46,7 +46,7 @@ function downLeftMovement(bishop, pawn) {
     let num = location.num;
     let alpha = location.alpha;
     while ((num < 7 && num > 0) && (alpha < 7 && alpha > 0)) {
-        num++;
+        num--;
         alpha--;
         if (chessBoard[num][alpha] === pawn) {
             return true;
@@ -62,7 +62,7 @@ function downRightMovement(bishop, pawn) {
     let num = location.num;
     let alpha = location.alpha;
     while ((num < 7 && num > 0) && (alpha < 7 && alpha > 0)) {
-        num++;
+        num--;
         alpha++;
         if (chessBoard[num][alpha] === pawn) {
             return true;
