0fe6293cf7ce4e2599817cdb6335c7bf7ebca576
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-04-10 22:29:58 +0530

    Added additional pawn movement test cases

--- 100-js-algo/__test__/bishopAndPawn.test.js
+++ 100-js-algo/__test__/bishopAndPawn.test.js
@@ -8,6 +8,24 @@ describe("Bishop and Pawn", () => {
         const actual = bishopAndPawn(bishopLocation, pawnLocation);
         expect(actual).toBeTruthy();
     });
+    test("Bishop capturing the pawn with Top Left Movement", () => {
+        const bishopLocation = "c3";
+        const pawnLocation = "a5";
+        const actual = bishopAndPawn(bishopLocation, pawnLocation);
+        expect(actual).toBeTruthy();
+    });
+    test("Bishop capturing the pawn with Down Right Movement", () => {
+        const bishopLocation = "a8";
+        const pawnLocation = "h1";
+        const actual = bishopAndPawn(bishopLocation, pawnLocation);
+        expect(actual).toBeTruthy();
+    });
+    test("Bishop capturing the pawn with Down Left Movement", () => {
+        const bishopLocation = "g7";
+        const pawnLocation = "d4";
+        const actual = bishopAndPawn(bishopLocation, pawnLocation);
+        expect(actual).toBeTruthy();
+    });
     test("Bishop not capturing the pawn", () => {
         const bishopLocation = "h1";
         const pawnLocation = "h3";
