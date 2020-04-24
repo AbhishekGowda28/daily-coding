bae31393ae0a7e72799e8cb20775558b1b21e31d
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-04-24 11:51:50 +0530

    Solved chess Board Cell Color problem

--- 100-js-algo/chessBoardCellColor.js
+++ 100-js-algo/chessBoardCellColor.js
@@ -1,5 +1,22 @@
+const chess = require("./utils/chess");
+
 function chessBoardCellColor(cell1, cell2) {
-    return undefined;
+    cell1 = cell1.toLowerCase();
+    cell2 = cell2.toLowerCase();
+    const cell1Loc = chess.findPawn(cell1);
+    const cell2Loc = chess.findPawn(cell2);
+
+    try {
+        const cell1Sum = cell1Loc.row + cell1Loc.column;
+        const cell2Sum = cell2Loc.row + cell2Loc.column;
+        if (cell1Sum === cell2Sum) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        return "Cell doesnot exist in the chessboard";
+    }
 }
 
 module.exports = { chessBoardCellColor };
