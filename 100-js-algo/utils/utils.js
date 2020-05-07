9e5c80267f52651b33166d1489ac8bd9e2acedab
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-05-07 22:52:00 +0530

    Added isEmpty method to utility

--- 100-js-algo/utils/utils.js
+++ 100-js-algo/utils/utils.js
@@ -19,7 +19,20 @@ function checkForElementInObject(object, element) {
     return data === undefined ? false : true;
 }
 
+function isEmpty(object) {
+    if (object !== undefined) {
+        if (object !== null) {
+            if (object !== {}) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 module.exports = {
     checkForElementInArray,
     checkForElementInObject,
+    isEmpty
 };
