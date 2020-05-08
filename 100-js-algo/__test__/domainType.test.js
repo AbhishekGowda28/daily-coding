182a226077f8ede1df9b1da8aa7e1fe1ccbd8c95
AbhishekGowda28 <abhi.gowda28@gmail.com>
2020-05-08 20:44:20 +0530

    Solved domain type problem

---
+++ 100-js-algo/__test__/domainType.test.js
@@ -0,0 +1,23 @@
+const expect = require("expect");
+const { domainType } = require("../domainType");
+
+describe("Test Domain list", () => {
+    test("Domain list", () => {
+        const domain = ["example.com"];
+        const actual = domainType(domain);
+        const expected = ["commercial"];
+        expect(actual).toStrictEqual(expected);
+    });
+    test("Domain does not exist", () => {
+        const domain = ["example.abc"];
+        const actual = domainType(domain);
+        const expected = "Domain does not exist";
+        expect(actual).toMatch(expected);
+    });
+    test("Domain does not exist", () => {
+        const domain = ["example.com", "abc.gov", "dreamerland.gov"];
+        const actual = domainType(domain);
+        const expected = ["commercial", "government"];
+        expect(actual).toStrictEqual(expected);
+    });
+});
