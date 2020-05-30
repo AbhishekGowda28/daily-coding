1319d07ef5ed1ce749dc80eea13b58adfe8ec23d
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-05-30 11:32:19 +0530

    Added get prime numbers

---
+++ 100-js-algo/getPrimeNumbers.js
@@ -0,0 +1,27 @@
+function getPrimeNumbers(range) {
+    let numbers = Array(range + 1).fill(true);
+    numbers[0] = false;
+    numbers[1] = false;
+    for (let count = 2; count <= range;) {
+        let nextFactor = count * count;
+        while (nextFactor <= range) {
+            if (numbers[nextFactor] === true) {
+                numbers[nextFactor] = false;
+            }
+            nextFactor += count;
+        }
+        count++;
+        while (numbers[count] === false) {
+            count++;
+        }
+    }
+    let primes = []
+    numbers.forEach((isPrime, index) => {
+        if (isPrime === true) {
+            primes.push(index);
+        }
+    });
+    return primes;
+}
+
+module.exports = { getPrimeNumbers };
