4c315fc42ed26a48fd331002f16cc877038430a8
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-06-15 19:49:58 +0530

    Renamed folder

--- object-understanding/anonymous-function.js
+++
@@ -1,9 +0,0 @@
-const Song = function (data = {}) {
-    this.name = data.name || "";
-}
-
-const newSong = new Song({name: "Blaha"});
-console.log("newSOng", newSong);
-
-const newSong23 = new Song();
-console.log(newSong23);
--- object-understanding/helper.js
+++
@@ -1,7 +0,0 @@
-function debugObject(object) {
-    for (const key in object) {
-        if (typeof object[key] !== "function") {
-            console.log(key, object[key]);
-        }
-    }
-}
