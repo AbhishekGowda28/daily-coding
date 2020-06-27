3d4d89f84c25c33319c9210b33cf1df7b9f4699d
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-06-27 20:18:17 +0530

    Acheived 100 % test coverage

--- 100-js-algo/utils/__test__/utils.test.js
+++ 100-js-algo/utils/__test__/utils.test.js
@@ -1,5 +1,5 @@
 const expect = require("expect");
-const { checkForElementInObject } = require("../utils");
+const { checkForElementInObject, isEmpty } = require("../utils");
 
 describe("Checking for element in object", () => {
     test("Presence of element", () => {
@@ -13,6 +13,49 @@ describe("Checking for element in object", () => {
         const inputObjet = { name: "" };
         const property = "firstName";
         const actual = checkForElementInObject(inputObjet, property);
+        expect(actual).toBeFalsy();
+    });
+});
+
+describe("Test suit to test is object empty", () => {
+    test("Object is undefined", () => {
+        const inputObject = undefined;
+        const actual = isEmpty(inputObject);
+        expect(actual).toBeTruthy();
+    });
+    test("Object is null", () => {
+        const inputObject = null;
+        const actual = isEmpty(inputObject);
+        expect(actual).toBeTruthy();
+    });
+    test("Object is {}", () => {
+        const inputObject = {};
+        const actual = isEmpty(inputObject);
+        expect(actual).toBeTruthy();
+    });
+    test("Object contains data", () => {
+        const inputObject = { name: "test" };
+        const actual = isEmpty(inputObject);
+        expect(actual).toBeFalsy();
+    });
+    test("Object is an array", () => {
+        const inputObject = [{ name: "test" }];
+        const actual = isEmpty(inputObject);
+        expect(actual).toBeFalsy();
+    });
+    test("Object is an empty array", () => {
+        const inputObject = [];
+        const actual = isEmpty(inputObject);
+        expect(actual).toBeTruthy();
+    });
+    test("Object is an empty string", () => {
+        const inputObject = "";
+        const actual = isEmpty(inputObject);
+        expect(actual).toBeTruthy();
+    });
+    test("Object is a string data", () => {
+        const inputObject = "Test";
+        const actual = isEmpty(inputObject);
         expect(actual).toBeFalsy();
     });
 });
