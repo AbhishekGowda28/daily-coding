f358ffb4715d6ea02d1bf85085c134454be08aea
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-07-08 02:21:22 +0530

    Removed typechecking from selection sort

--- sorting/src/utils/selectionSort.js
+++ sorting/src/utils/selectionSort.js
@@ -1,6 +1,7 @@
-// @ts-check
-
 function* yeildSelectionSort(inputArray) {
+    if (typeof inputArray === "string") {
+        inputArray = inputArray.split("");
+    }
     for (let index = 0; index < inputArray.length; index++) {
         let leastValueIndex = index;
         let elementCounter = index + 1;
@@ -20,9 +21,6 @@ function* yeildSelectionSort(inputArray) {
     return inputArray;
 }
 
-/**
- * @param {any[] | string} inputArray
- */
 function selectionSort(inputArray) {
     if (typeof inputArray === "string") {
         inputArray = inputArray.split("");
@@ -46,16 +44,3 @@ function selectionSort(inputArray) {
 }
 
 export { yeildSelectionSort, selectionSort };
-
-// let result = selectionSort([]);
-// console.log(result)
-// result = selectionSort("");
-// console.log(result)
-// result = selectionSort([1, 5, 3, 4, 2]);
-// console.log(result)
-// result = selectionSort(["a", "b", "h", "i", "s", "h", "e", "k"]);
-// console.log(result)
-// result = selectionSort("gowda");
-// console.log(result)
-// result = selectionSort("Abhishek Gowda");
-// console.log(result)
