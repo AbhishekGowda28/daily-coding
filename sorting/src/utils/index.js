16d159e61fa01a03da0729926f54190aa43b1129
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-07-08 02:21:42 +0530

    Added try catch block inside getRandomColor function

--- sorting/src/utils/index.js
+++ sorting/src/utils/index.js
@@ -1,9 +1,16 @@
 export default function getRandomColor(lightness) {
-    let hue = 200;
-    while (lightness > 90) {
-        hue += 15;
-        lightness -= 30;
+    try {
+        if (typeof lightness === "string") {
+            lightness = lightness.charCodeAt(0);
+        }
+        let hue = 200;
+        while (lightness > 90) {
+            hue += 15;
+            lightness -= 30;
+        }
+        lightness = 100 - lightness;
+        return `hsl(${hue}, 100%, ${lightness}%)`;
+    } catch (error) {
+        throw new Error("Invalid Input for lightness");
     }
-    lightness = 100 - lightness;
-    return `hsl(${hue}, 100%, ${lightness}%)`;
 }
