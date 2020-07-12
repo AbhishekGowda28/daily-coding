38c2d4586163efec052fa3f59a77df1e021eb835
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-07-12 20:27:00 +0530

    Sorted imports in Checkbox test

--- sorting/src/components/Common/__test__/Checkbox.test.jsx
+++ sorting/src/components/Common/__test__/Checkbox.test.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { cleanup, fireEvent, render } from '@testing-library/react';
+import React from 'react';
 import CheckBox from '../Checkbox';
 
 describe("Testing Check Box Component", () => {
