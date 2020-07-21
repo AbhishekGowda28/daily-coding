ee5e15226cec113a5a97268aa13f164cf6f6c0e0
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-07-21 21:34:35 +0530

    Completed  Fermat's factorization problem

---
+++ 100-js-algo/fermatFactorization.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+const { isSquare } = require("./utils/Math/isSquare");
+
+/**
+ * @param {number} number
+ */
+function fermatFactorization(number) {
+    let firstNumber = Math.ceil(Math.sqrt(number));
+    let secondNumber = firstNumber * firstNumber - number;
+    while (isSquare(secondNumber) === false) {
+        firstNumber = firstNumber + 1;
+        secondNumber = firstNumber * firstNumber - number;
+    }
+
+    return [firstNumber, Math.sqrt(secondNumber)];
+}
+
+module.exports = { fermatFactorization };
