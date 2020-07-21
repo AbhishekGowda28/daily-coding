d937c94affbf505dbb2db753894e38181d9e63e9
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-07-21 22:04:51 +0530

    Update based on codeMetrics

--- 100-js-algo/angleType.js
+++ 100-js-algo/angleType.js
@@ -1,17 +1,18 @@
 function angleType(angle) {
+    let returnAngle = "Angle is more than 180";
     if (angle < 0) {
-        return "Angle is less than 0";
+        returnAngle = "Angle is less than 0";
     } else if (angle < 90) {
-        return "acute";
+        returnAngle = "acute";
     } else if (angle === 90) {
-        return "right";
+        returnAngle = "right";
     } else if (angle === 180) {
-        return "straight";
+        returnAngle = "straight";
     } else if (angle > 90 && angle < 180) {
-        return "obtuse";
-    } else {
-        return "Angle is more than 180";
+        returnAngle = "obtuse";
     }
+
+    return returnAngle;
 }
 
 module.exports = { angleType };
