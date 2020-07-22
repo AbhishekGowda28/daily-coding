a604b79b1f1d2653b6181b8168da94c4ef1eed25
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-07-22 19:23:44 +0530

    Refactored code based on input from code-metrics

--- 100-js-algo/encloseInBrackets.js
+++ 100-js-algo/encloseInBrackets.js
@@ -2,13 +2,15 @@ function encloseInBrackets(inputStr) {
     if (typeof inputStr === "string") {
         if (inputStr[0] === "(" && inputStr[inputStr.length - 1] === ")") {
             return inputStr;
-        } else if (inputStr[0] === "(" && inputStr[inputStr.length - 1] !== ")") {
-            return inputStr + ")";
-        } else if (inputStr[0] !== "(" && inputStr[inputStr.length - 1] === ")") {
-            return "(" + inputStr;
-        } else {
-            return "(" + inputStr + ")";
         }
+        if (inputStr[0] === "(" && inputStr[inputStr.length - 1] !== ")") {
+            return inputStr + ")";
+        }
+        if (inputStr[0] !== "(" && inputStr[inputStr.length - 1] === ")") {
+            return "(" + inputStr;
+        }
+
+        return "(" + inputStr + ")";
     }
     throw new Error("Invalid Input");
 }
