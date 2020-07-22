660c5ec8d2d09fd5f0648647427b04706461d030
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-07-22 19:48:53 +0530

    refactored fileNaming problem

--- 100-js-algo/fileNaming.js
+++ 100-js-algo/fileNaming.js
@@ -1,28 +1,48 @@
+/* eslint-disable max-params */
+// @ts-check
+/**
+ * @param {string} newName
+ * @param {string} fileName
+ * @param {string | string[]} outputFileNames
+ */
+function incrementDuplicateFileCount(newName, fileName, outputFileNames) {
+    let checker = true;
+    while (checker) {
+        const extractNumber = /.*?\((\d)\)/;
+        const data = extractNumber.exec(newName);
+        let nextFileName = Number(data[1]);
+        nextFileName++;
+        newName = `${fileName}(${nextFileName})`;
+        checker = outputFileNames.includes(newName);
+    }
+
+    return newName;
+}
+
+/**
+ * @param {string} fileName
+ * @param {string | string[]} outputFileNames
+ */
+function updateDuplicateFile(fileName, outputFileNames) {
+    let newName = `${fileName}(1)`;
+    if (outputFileNames.includes(newName)) {
+        newName = incrementDuplicateFileCount(newName, fileName, outputFileNames);
+    }
+
+    return newName;
+}
+
+/**
+ * @param {string[]} fileNames
+ */
 function fileNaming(fileNames) {
     if (Array.isArray(fileNames)) {
         const outputFileNames = [];
         fileNames.forEach((fileName) => {
-            if (outputFileNames.includes(fileName) === false) {
-                outputFileNames.push(fileName);
-            } else {
-                let newName = `${fileName}(1)`;
-                if (outputFileNames.includes(newName) === false) {
-                    outputFileNames.push(newName);
-                } else {
-                    let checker = true;
-                    while (checker) {
-                        const extractNumber = /.*?\((\d)\)/;
-                        const data = extractNumber.exec(newName);
-                        let nextFileName = Number(data[1]);
-                        nextFileName++;
-                        newName = `${fileName}(${nextFileName})`;
-                        if (outputFileNames.includes(newName) === false) {
-                            checker = false;
-                        }
-                    }
-                    outputFileNames.push(newName);
-                }
+            if (outputFileNames.includes(fileName)) {
+                fileName = updateDuplicateFile(fileName, outputFileNames);
             }
+            outputFileNames.push(fileName);
         });
 
         return outputFileNames;
