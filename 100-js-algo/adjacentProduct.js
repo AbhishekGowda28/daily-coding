efca0925ab5fc6e6776d5ae649977e18984a7356
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-07-22 18:45:45 +0530

    Refactored adjacentProduct problem

--- 100-js-algo/adjacentProduct.js
+++ 100-js-algo/adjacentProduct.js
@@ -1,19 +1,38 @@
-function adjacentProduct(matrix) {
-	const initalValue = matrix.length > 1 ?
+/* eslint-disable max-params */
+
+/**
+ * @param {number[]} matrix
+ */
+function getInitalValue(matrix) {
+	return matrix.length > 1 ?
 		matrix[0] * matrix[1] : matrix.length === 1 ?
 			matrix[0] : 0;
+}
 
-	// eslint-disable-next-line max-params
-	return matrix.reduce((totalProd, element, index) => {
-		if (index < matrix.length - 1) {
-			const product = element * matrix[index + 1];
-			if (product > totalProd) {
-				return product;
-			}
+/**
+ * @param {number} totalProd
+ * @param {number} element
+ * @param {number} index
+ * @param {number[]} matrix
+ */
+function matrixReduction(totalProd, element, index, matrix) {
+	if (index < matrix.length - 1) {
+		const product = element * matrix[index + 1];
+		if (product > totalProd) {
+			return product;
 		}
+	}
 
-		return totalProd;
-	}, initalValue);
+	return totalProd;
+}
+
+/**
+ * @param {number[]} matrix
+ */
+function adjacentProduct(matrix) {
+	const initalValue = getInitalValue(matrix);
+
+	return matrix.reduce(matrixReduction, initalValue);
 }
 
 module.exports = {
