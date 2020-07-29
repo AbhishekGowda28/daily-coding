234be33d912d37abd17fe5959d1632ab70e2f8f9
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-07-29 18:38:03 +0530

    Completed Reverse Parenthese problem

---
+++ 100-js-algo/__test__/reverseParentheses.test.js
@@ -0,0 +1,28 @@
+const expect = require("expect");
+const { reverseParentheses } = require("../reverseParentheses");
+
+describe("Reverse of Parenthese", () => {
+    test("Single Parenthese", () => {
+        const inputString = "a(bc)d";
+        const expected = "acbd";
+        const actual = reverseParentheses(inputString);
+        expect(actual).toEqual(expected);
+    });
+    test("Parenthese strating of the string", () => {
+        const inputString = "(a(bc)d)";
+        const expected = "dbca";
+        const actual = reverseParentheses(inputString);
+        expect(actual).toEqual(expected);
+    });
+    test("Double Parenthese strating of the string", () => {
+        const inputString = "((a(bc)d))";
+        const expected = "acbd";
+        const actual = reverseParentheses(inputString);
+        expect(actual).toEqual(expected);
+    });
+    test("No Parenthese", () => {
+        const inputString = "abcd";
+        const actual = reverseParentheses(inputString);
+        expect(actual).toEqual(inputString);
+    });
+});
--- 100-js-algo/reverseParentheses.js
+++ 100-js-algo/reverseParentheses.js
@@ -6,32 +6,14 @@ const { reverse } = require("./utils/string");
  * @param {string} inputString
  */
 function reverseParentheses(inputString) {
-    // const parentheseRegex = /\((.*)\)/;
-    // const intermediateStr = inputString;
-    // let result = parentheseRegex.exec(intermediateStr);
-    // console.log(result);
-    // let endStr = "";
-    // while (result !== null) {
-    //     console.log("begining", result);
-    //     endStr += intermediateStr.split("(")[0];
-    //     endStr += reverse(result[1]);
-    //     endStr += intermediateStr.substring(intermediateStr.lastIndexOf(")") + 1);
-    //     result = parentheseRegex.exec(endStr);
-    //     console.log("end", result, endStr);
-    // }
-    // return endStr;
-
-    const regex = /(.*?)\((.*)\)(.*)/;
-    while (regex.test(inputString)) {
-        const result = regex.exec(inputString);
+    const regex = /(.*?)\(([a-zA-Z0-9_]*)\)(.*)/;
+    let result = regex.exec(inputString);
+    while (result !== null) {
         inputString = result[1] + reverse(result[2]) + result[3];
+        result = regex.exec(inputString);
     }
 
     return inputString;
 }
-
-console.log(reverseParentheses("a(bc)d"));
-
-console.log(reverseParentheses("((a(bc)d))"));
 
 module.exports = { reverseParentheses };
