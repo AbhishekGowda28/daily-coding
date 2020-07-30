7b5931edacbd9b3133f22a564e511e15efccc77d
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-07-30 13:26:29 +0530

    Completed lucky ticket problem

---
+++ 100-js-algo/luckyTicket.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+const { rootNumberSum } = require(`./utils/Math/rootNumber`);
+
+/**
+ * @param {number} ticketNumber
+ */
+function luckyTicket(ticketNumber) {
+    if (Number(ticketNumber) % 2 === 0) {
+        const mid = String(ticketNumber).length / 2;
+        const leftHalf = rootNumberSum(String(ticketNumber).slice(0, mid));
+        const rightHalf = rootNumberSum(String(ticketNumber).slice(mid));
+
+        return leftHalf === rightHalf;
+    }
+
+    return false;
+}
+
+module.exports = { luckyTicket };
