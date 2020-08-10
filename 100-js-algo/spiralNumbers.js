f165f7949bac65a7b7b7b860c59067774bdee2a3
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-08-10 23:49:27 +0530

    Added Spiral array problem statement

--- 100-js-algo/spiralNumbers.js
+++ 100-js-algo/spiralNumbers.js
@@ -4,7 +4,12 @@
  * @param {number} num
  */
 function spiralNumbers(num) {
+    const sprialArray = [];
+    for (let count = 0; count < num; count++) {
+        for (let col = 0; col < num; col++) {
 
+        }
+    }
 }
 
 module.exports = spiralNumbers;
