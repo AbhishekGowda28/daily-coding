6f1a57f28230aa107c58434e4a811b2c0d9daecb
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-08-30 23:22:14 +0530

    update on getting started

--- node/getting-started.js
+++ node/getting-started.js
@@ -1,20 +1,21 @@
 const http = require("http");
 
 const hostName = "localhost";
-const port = 5002;
+const portNumber = 5002;
 
 // Arrow functions do not have this associated with anything.
 // On console log it would return undefined
 // But, if its bind to any object they Arrow function would incorporate the properties
 
-const requestListiner = (props) => {
-    console.log("requestListiner -> properties 😁", props);
+const requestListiner = (_, res) => {
+    res.writeHead(200, { "content-Type": "text/plain" })
+    res.end("ok")
 };
 
 const server = http.createServer(requestListiner);
 
 const listeningListiner = () => {
-    console.log("Server Started");
+    console.log(`Server Running at http://${hostName}:${portNumber}`);
 };
 
-server.listen(port, hostName, listeningListiner);
+server.listen(portNumber, hostName, listeningListiner);
