672c36a4ca61ce62ae2c72e79257037ee001f130
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-08-31 23:18:49 +0530

    Added sample application

---
+++ node_with_electron/getting_started_with_electron/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+    const electronWindow = new BrowserWindow({
+        width: 200,
+        height: 150,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    electronWindow.loadFile("index.html");
+}
+
+app.whenReady().
+    then(createWindow).
+    catch(e => {
+        console.log("Error");
+    });
+
+app.on("window-all-closed", () =>{
+    if(process.platform !== "darwin"){
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if(BrowserWindow.getAllWindows().length === 0){
+        createWindow();
+    }
+});
