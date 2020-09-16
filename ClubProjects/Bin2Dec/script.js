ae1b7a1fe95441e4536f44e386467d48c79456c6
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-09-16 22:53:06 +0530

    Completed Bin2Dec with bonus feature

--- ClubProjects/Bin2Dec/script.js
+++ ClubProjects/Bin2Dec/script.js
@@ -1,13 +1,17 @@
 const binaryInput = document.querySelector("input#binary");
 const decimalInput = document.querySelector("input#decimal");
+const error = document.querySelector("div.error");
 let previousInput = "";
 binaryInput.addEventListener("input", (event) => {
+    error.innerHTML = "";
     const heldInput = event.target.value;
     event.target.value = previousInput;
     if (validateInput(heldInput)) {
         event.target.value = heldInput;
         previousInput = heldInput;
         decimalInput.value = convertBinaryToDecimal(heldInput);
+    } else {
+        error.innerHTML = "Invalid Input. Only Binary Numbers [0 or 1] are Allowed"
     }
 });
 
@@ -16,7 +20,7 @@ function validateInput(heldInput) {
 }
 
 function convertBinaryToDecimal(binary) {
-     binary = (Array.from(binary.split("")).reverse()).join("");
+    binary = (Array.from(binary.split("")).reverse()).join("");
     let binaryLength = binary.length;
     let decimalValue = 0;
     while (binaryLength > 0) {
