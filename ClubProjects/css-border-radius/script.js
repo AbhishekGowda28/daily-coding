67c375b0cdfed59ea3b16c920751760b0324d29a
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-09-20 21:02:50 +0530

    Extracted out event functions

--- ClubProjects/css-border-radius/script.js
+++ ClubProjects/css-border-radius/script.js
@@ -5,19 +5,23 @@ const leftMarker = document.querySelector("div.marker.left input");
 const rightMarker = document.querySelector("div.marker.right input");
 const parentContainer = document.getElementById("container");
 const styleContainer = document.getElementById("style-container");
+
 let text = `border-radius: ${Number(topMarker.value)}px ${Number(rightMarker.value)}px ${Number(bottomMarker.value)}px ${Number(leftMarker.value)}px;`;
 
 styleContainer.innerText = text;
 
-copy.addEventListener("click", () => {
-    navigator.clipboard.writeText(text);
-});
+copy.addEventListener("click", CopyStyle);
+parentContainer.addEventListener("input", SliderInput);
 
-parentContainer.addEventListener("input", (_event) => {
+function CopyStyle(_event) {
+    navigator.clipboard.writeText(text);
+}
+
+function SliderInput(_event) {
     styleContainer.innerText = `border-radius: ${Number(topMarker.value)}px ${Number(rightMarker.value)}px ${Number(bottomMarker.value)}px ${Number(leftMarker.value)}px;`;
     text = styleContainer.innerText;
     parentContainer.style.borderTopLeftRadius = `${Number(topMarker.value)}px`
     parentContainer.style.borderTopRightRadius = `${Number(rightMarker.value)}px`
     parentContainer.style.borderBottomLeftRadius = `${Number(leftMarker.value)}px`
     parentContainer.style.borderBottomRightRadius = `${Number(bottomMarker.value)}px`
-});
+}
