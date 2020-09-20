c149cc72489a2dad23591e3e0f04203d05a1de2d
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-09-20 15:48:32 +0530

    Completed CSS Border Radius

--- ClubProjects/css-border-radius/script.js
+++ ClubProjects/css-border-radius/script.js
@@ -13,25 +13,11 @@ copy.addEventListener("click", () => {
     navigator.clipboard.writeText(text);
 });
 
-parentContainer.addEventListener("input", (event) => {
-    styleContainer.innerText = `
-        border-radius: ${Number(topMarker.value)}px ${Number(rightMarker.value)}px ${Number(bottomMarker.value)}px ${Number(leftMarker.value)}px;
-    `;
+parentContainer.addEventListener("input", (_event) => {
+    styleContainer.innerText = `border-radius: ${Number(topMarker.value)}px ${Number(rightMarker.value)}px ${Number(bottomMarker.value)}px ${Number(leftMarker.value)}px;`;
     text = styleContainer.innerText;
     parentContainer.style.borderTopLeftRadius = `${Number(topMarker.value)}px`
     parentContainer.style.borderTopRightRadius = `${Number(rightMarker.value)}px`
     parentContainer.style.borderBottomLeftRadius = `${Number(leftMarker.value)}px`
     parentContainer.style.borderBottomRightRadius = `${Number(bottomMarker.value)}px`
-});
-
-
-parentContainer.addEventListener("dragstart", (event) => {
-    console.log("event Details", event);
-    let currentValue = event.target.parentElement.style.left !== "" ? Number((event.target.parentElement.style.left).substring(0, event.target.parentElement.style.left.length - 1)) : 0;
-    console.log("event.target", JSON.stringify(event.x));
-    event.target.parentElement.style.left = `${currentValue + 1}%`;
-});
-
-parentContainer.addEventListener("drop", (dragEndEvent) => {
-    console.log("Drag End", dragEndEvent);
 });
