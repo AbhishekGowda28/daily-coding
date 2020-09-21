75856841397a2cd0b31cb5c78c3dc5e99ccb5666
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-09-21 12:28:16 +0530

    update on hover effect

--- ClubProjects/on-hover-effect/scripts/script.js
+++ ClubProjects/on-hover-effect/scripts/script.js
@@ -1,10 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
-    async function a() {
-        const data = await fetch("./scripts/assets.json").then((result) => {
+    async function getAssets() {
+        return await fetch("./scripts/assets.json").then((result) => {
             return result.json();
         }).then(res => res);
-        return data;
-    };
-    const data = a();
-    console.log(data);
-})
+    }
+
+    getAssets().then((assets) => {
+        const appContainer = document.getElementById("app-container");
+        assets.forEach(asset => {
+            const assetContainer = document.createElement("div");
+            assetContainer.className = "asset";
+            const image = document.createElement("img");
+            image.src = asset.src;
+            const span = document.createElement("span");
+            span.innerText= `@${asset.author}`;
+            assetContainer.appendChild(image);
+            assetContainer.appendChild(span)
+            appContainer.append(assetContainer);
+        });
+    });
+});
