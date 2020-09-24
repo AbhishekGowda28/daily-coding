fdcf423cfabe01c76d7b55197e5782bbd051a249
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-09-24 21:53:50 +0530

    Re-created using redux template

--- ClubProjects/calculator/src/index.js
+++ ClubProjects/calculator/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import { applyMiddleware } from "redux";
 import './index.css';
 import App from './App';
-import { calculatorStore } from './store/sample';
+import store from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider strore={applyMiddleware (calculatorStore)}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
