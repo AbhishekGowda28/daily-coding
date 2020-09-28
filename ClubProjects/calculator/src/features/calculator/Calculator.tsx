d61de2027256082eaa3636f5acd8e88c4bbbce25
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-09-28 22:00:51 +0530

    Started with the calculator app

--- ClubProjects/calculator/src/features/calculator/Calculator.tsx
+++ ClubProjects/calculator/src/features/calculator/Calculator.tsx
@@ -1,108 +1,38 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  selectCount,
-} from './calculatorSlice';
-import styles from './Calculator.module.css';
 
 export function Calculator() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const [previousState, setPreviousState] = useState("");
+  const [result, setResult] = useState(0);
+  const [operator, setOperator] = useState("");
 
   return (
     <div>
-      {/* <div className="calculator">
-        <div className="previous-actions">
-          ABCD
+      <div className="calculator">
+        <div className="previous-state">
+          {previousState}
         </div>
-        <div className="current-value">
-          EFG
-        </div>
-        <div>
-          <span>MC</span>
-          <span>M+</span>
-          <span>M-</span>
-          <span>MR</span>
+        <div className="result">
+          {result}
         </div>
         <div>
-          <span>C</span>
-          <span>+/-</span>
-          <span>/-</span>
-          <span>X</span>
+          <button onClick={(value) => {
+            setResult(Number(result + value.currentTarget.innerText));
+          }}>1</button>
+          <button onClick={(value) => {
+            const prev = String(result) + "+";
+            setPreviousState(prev);
+            setResult(0);
+            setOperator("+")
+          }}>+</button>
+          <button onClick={(value) => {
+            const lhs = previousState.split(operator)[0];
+            const rhs = result;
+            const final = (Number(lhs) + Number(rhs));
+            setResult(final);
+            setPreviousState(lhs + "+" + rhs);
+          }}>=</button>
         </div>
-        <div>
-          <span>7</span>
-          <span>8</span>
-          <span>9</span>
-          <span>-</span>
-        </div>
-        <div>
-          <span>4</span>
-          <span>5</span>
-          <span>6</span>
-          <span>+</span>
-        </div>
-        <div>
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>=</span>
-        </div>
-        <div>
-          <span>x</span>
-          <span>0</span>
-          <span>.</span>
-        </div>
-        <div>
-        </div>
-      </div> */}
-
-
-
-
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() =>
-            dispatch(incrementByAmount(Number(incrementAmount) || 0))
-          }
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
-        >
-          Add Async
-        </button>
       </div>
     </div>
   );
