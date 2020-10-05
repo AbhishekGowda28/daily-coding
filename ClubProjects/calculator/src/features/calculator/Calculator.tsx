fc70122feb5b4261a392ab6c966ba856dbeaa2ed
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-10-05 20:41:51 +0530

    Completed calculator

--- ClubProjects/calculator/src/features/calculator/Calculator.tsx
+++ ClubProjects/calculator/src/features/calculator/Calculator.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import "./calculator.css"
 export function Calculator() {
 
-  const [previousState, setPreviousState] = useState("");
+  const [previousResult, setPreviousResult] = useState("");
   const [result, setResult] = useState("0");
   const [operator, setOperator] = useState("");
   const [sign, setSign] = useState("");
@@ -13,7 +13,7 @@ export function Calculator() {
   };
 
   const clearInput = () => {
-    setPreviousState("");
+    setPreviousResult("");
     setResult("0");
     setOperator("");
     setSign("");
@@ -27,9 +27,52 @@ export function Calculator() {
     }
   };
 
+  const assignResultToPreviousResult = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setPreviousResult(result);
+    setResult("");
+    setOperator(event.currentTarget.innerText);
+  }
+
+  const evaluate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    debugger;
+    if (operator !== "") {
+      const prev = Number(previousResult);
+      const curr = Number(result);
+      switch (operator) {
+        case "+": {
+          setResult((prev + curr).toString());
+          setPreviousResult("");
+          setOperator("");
+          break;
+        }
+        case "-": {
+          setResult((prev - curr).toString());
+          setPreviousResult("");
+          setOperator("");
+          break;
+        }
+        case "x": {
+          setResult((prev * curr).toString());
+          setPreviousResult("");
+          setOperator("");
+          break;
+        }
+        case "/": {
+          setResult((prev / curr).toString());
+          setPreviousResult("");
+          setOperator("");
+          break;
+        }
+        default: break;
+      }
+
+    }
+  }
+
+
   return (
     <div className="calculator">
-      <div className="calculation">{previousState}</div>
+      <div className="calculation">{previousResult}</div>
       <div className="result">
         <span>{sign}</span>
         <span>{result}</span>
@@ -39,30 +82,33 @@ export function Calculator() {
           <div className="control" onClick={clearInput}>C</div>
           <div className="control" onClick={toggleSign}>+/-</div>
           <div className="control">%</div>
-          <div className="control">&divide;</div>
+          {/* <div className="control" onClick={assignResultToPreviousResult}>&divide;</div> */}
+          <div className="control" onClick={assignResultToPreviousResult}>/</div>
         </div>
         <div className="row">
           <div className="control" onClick={handleNumberInput}>7</div>
           <div className="control" onClick={handleNumberInput}>8</div>
           <div className="control" onClick={handleNumberInput}>9</div>
-          <div className="control">&times;</div>
+          {/* <div className="control" onClick={assignResultToPreviousResult}>&times;</div> */}
+          <div className="control" onClick={assignResultToPreviousResult}>x</div>
         </div>
         <div className="row">
           <div className="control" onClick={handleNumberInput}>4</div>
           <div className="control" onClick={handleNumberInput}>5</div>
           <div className="control" onClick={handleNumberInput}>6</div>
-          <div className="control">&minus;</div>
+          {/* <div className="control" onClick={assignResultToPreviousResult}>&minus;</div> */}
+          <div className="control" onClick={assignResultToPreviousResult}>-</div>
         </div>
         <div className="row">
           <div className="control" onClick={handleNumberInput}>1</div>
           <div className="control" onClick={handleNumberInput}>2</div>
           <div className="control" onClick={handleNumberInput}>3</div>
-          <div className="control">+</div>
+          <div className="control" onClick={assignResultToPreviousResult}>+</div>
         </div>
         <div className="row">
           <div className="control zero" onClick={handleNumberInput}>0</div>
           <div className="control" onClick={handleNumberInput}>.</div>
-          <div className="control equals">=</div>
+          <div className="control equals" onClick={evaluate}>=</div>
         </div>
       </div>
     </div>
