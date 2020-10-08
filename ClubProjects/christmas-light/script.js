dc8386dcb5777124c8a76b177a3b74f770058db4
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-10-08 18:48:49 +0530

    Alternating christmas lights

--- ClubProjects/christmas-light/script.js
+++ ClubProjects/christmas-light/script.js
@@ -1,5 +1,5 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+    let count = 0;
     const bulbs = document.getElementsByClassName("blub");
     const playButton = document.querySelector(".controller button");
     playButton.addEventListener("click", (event) => {
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (text === "Start") {
             event.currentTarget.value = "Stop"
             playButton.innerText = "Stop";
-            start(bulbs);
+            startBlub();
         } else {
             event.currentTarget.value = "Start"
             playButton.innerText = "Start";
@@ -15,15 +15,25 @@ document.addEventListener("DOMContentLoaded", () => {
                 let bulb = bulbs[bulbCount];
                 bulb.classList.replace(`lite-${bulbCount + 1}`, `light-${bulbCount + 1}`)
             }
+            clearInterval(timer);
         }
     });
 
-});
-
-function start(bulbs) {
-    for (let bulbCount = 0; bulbCount < bulbs.length; bulbCount++) {
-        let bulb = bulbs[bulbCount];
-        bulb.classList.replace(`light-${bulbCount + 1}`, `lite-${bulbCount + 1}`);
+    function start() {
+        for (let bulbCount = 0; bulbCount < bulbs.length; bulbCount++) {
+            let bulb = bulbs[bulbCount];
+            if (bulbCount % 2 === count) {
+                bulb.classList.replace(`light-${bulbCount + 1}`, `lite-${bulbCount + 1}`);
+            } else {
+                bulb.classList.replace(`lite-${bulbCount + 1}`, `light-${bulbCount + 1}`)
+            }
+        }
+        count = (count + 1) % 2;
     }
-}
+    let timer;
 
+    function startBlub() {
+        timer = setInterval(start, 500);
+    }
+
+});
