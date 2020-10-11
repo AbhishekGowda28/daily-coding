5002b0415145b1776600130998462ee3fd49d316
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-10-11 22:34:17 +0530

    Added structure to the project

---
+++ ClubProjects/causeEffect/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const usersUrl = "https://my-json-server.typicode.com/abhishekgowda28/sampledata/users";
+
+    const userListContainer = document.getElementById("users-list");
+
+    async function getUsersList() {
+        const users = await fetch(usersUrl);
+        const usersList = await users.json();
+        return usersList;
+    }
+
+    function displayUserList(userDetails) {
+        const user = document.createElement("ul");
+        user.innerText = userDetails.first_name;
+        userListContainer.appendChild(user);
+        user.addEventListener("click", (event) => {
+            event.stopPropagation();
+            let leftPane = document.getElementById("left-pane");
+            leftPane.innerHTML = "";
+            const userDetailsPane = document.createElement("div");
+            userDetailsPane.innerHTML = `<div><span>ID</span>${userDetails.id}</div><div><span> Name </span>${userDetails.first_name} ${userDetails.last_name}</div>`;
+            leftPane.appendChild(userDetailsPane);
+        });
+    }
+
+    function updateUserList() {
+        getUsersList().then((users) => {
+            users.forEach((userDetails) => {
+                displayUserList(userDetails);
+            });
+        });;
+    }
+
+    updateUserList();
+
+});
