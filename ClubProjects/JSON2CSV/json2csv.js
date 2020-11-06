65e8f48c61e79795f0bf367490f8d8dcb8f7ceb5
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-11-06 22:26:01 +0530

    JSON to CSV completed

---
+++ ClubProjects/JSON2CSV/json2csv.js
@@ -0,0 +1,30 @@
+function JSON2CSV(json) {
+    const keys = Object.keys(json);
+    const values = Object.values(json);
+    let keyStr = "";
+    keys.forEach((key) => {
+        keyStr += `${key}\t`;
+    });
+    keyStr += "\n";
+    let valueStr = "";
+    values.forEach((value) => {
+        valueStr += `${JSON.stringify(value)}\t`;
+    });
+    valueStr += "\n";
+    return `${keyStr}\n${valueStr}`;
+}
+
+const SAMPLE_JSON = {
+    a: 1,
+    name: "ABCD",
+    age: "123",
+    string: ["a", "w"],
+    number: [1, 2, 4, 5],
+    ppl: {
+        name: "23",
+        age: 23
+    },
+    date: new Date().toLocaleString()
+};
+
+console.log(JSON2CSV(SAMPLE_JSON));;
