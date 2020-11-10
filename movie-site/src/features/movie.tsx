77108c23d4e9dacfe701e0ea07a07548eed4a46d
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-11-10 21:09:54 +0530

    Removed un-nesseray files

--- movie-site/src/features/movie.tsx
+++ movie-site/src/features/movie.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import store from "../app/store";
+import { getMovieList } from "./movieSlice";
 export const Movie = () => {
     return (
-        <div onClick={store.dispatch}>Moives</div>
+        <div onClick={store.dispatch(getMovieList)}>Moives</div>
     );
 }
