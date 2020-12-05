fb10b0b0548e0c258e5aaacc6d28fba844bd697e
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-12-05 22:21:19 +0530

    working with google oauth

---
+++ Auth/google/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("login-button");
+    button.addEventListener("click", () => {
+        console.log("Clicked on login button");
+    });
+});
