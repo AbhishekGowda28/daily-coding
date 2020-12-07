622e76c56dacc0e7e922faf02aa0437f816c264b
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-12-07 21:27:32 +0530

    Authorization

--- Auth/google/script.js
+++ Auth/google/script.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("login-button");
+    const googleClientID = "550963546957-8af6kn0flq4bnbmsfvt8go1rp53mdp09.apps.googleusercontent.com";
+    const googleClientSecret = "TMyogdCYkTuErmQ0jdszX6R6";
+    const authorizeURL = 'https://accounts.google.com/o/oauth2/v2/auth';
+    const tokenURL = 'https://www.googleapis.com/oauth2/v4/token';
+    const baseURL = "http:localhost:5500";
     button.addEventListener("click", () => {
         console.log("Clicked on login button");
+        fetch(authorizeURL, { mode: "no-cors" }).
+            then(result => {
+                console.log(result);
+            }).
+            catch(error => {
+                console.error("Error", error);
+            });
     });
 });
