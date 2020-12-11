69fd0842fd333a74002897018a06159774bf5cbb
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-12-11 22:19:20 +0530

    Converted JS file to TS files

--- node-projects/user-profile/dist/index.js
+++ node-projects/user-profile/dist/index.js
@@ -1,10 +1,8 @@
 let mango = require("mongodb");
 const mongoClient = mango.MongoClient;
-
 const MONGO_URL = "mongodb://localhost:27017/";
 let DATABASE;
 const collections = [];
-
 function createDatabase(databaseName) {
     const URL = `${MONGO_URL}/${databaseName}`;
     mongoClient.connect(URL, (error, db) => {
@@ -17,41 +15,44 @@ function createDatabase(databaseName) {
         db.close();
     });
 }
-
 function createCollection(collectionName) {
     mongoClient.connect(MONGO_URL, function (err, db) {
-        if (err) throw err;
+        if (err)
+            throw err;
         const dbo = db.db(DATABASE);
         dbo.createCollection(collectionName, function (err, res) {
-            if (err) throw err;
+            if (err)
+                throw err;
             console.log("Collection created!");
             collections.push(collectionName);
             db.close();
         });
     });
 }
-
-function insertRecord(record){
+function insertRecord(record) {
     mongoClient.connect(MONGO_URL, function (err, db) {
-        if (err) throw err;
+        if (err)
+            throw err;
         const dbo = db.db(DATABASE);
         dbo.collection(collections[0]).insertOne(record, function (err, res) {
-            if (err) throw err;
+            if (err)
+                throw err;
             console.log("1 document inserted");
             db.close();
         });
-    }); 
+    });
 }
-
-function insertRecords(records){
-
+function insertRecords(records) {
     mongoClient.connect(MONGO_URL, function (err, db) {
-        if (err) throw err;
+        if (err)
+            throw err;
         const dbo = db.db(DATABASE);
         dbo.collection(collections[0]).insertMany(records, function (err, res) {
-            if (err) throw err;
+            if (err)
+                throw err;
             console.log("Number of documents inserted: " + res.insertedCount);
             db.close();
         });
-    }); 
+    });
 }
+//# sourceMappingURL=index.js.map
---
+++ node-projects/user-profile/src/index.ts
@@ -0,0 +1,57 @@
+let mango = require("mongodb");
+const mongoClient = mango.MongoClient;
+
+const MONGO_URL: string = "mongodb://localhost:27017/";
+let DATABASE: string;
+const collections: string[] = [];
+
+function createDatabase(databaseName: string) {
+    const URL = `${MONGO_URL}/${databaseName}`;
+    mongoClient.connect(URL, (error: any, db: { close: () => void; }) => {
+        if (error) {
+            console.error(error);
+            throw error;
+        }
+        console.log("DB created");
+        DATABASE = databaseName;
+        db.close();
+    });
+}
+
+function createCollection(collectionName: string) {
+    mongoClient.connect(MONGO_URL, function (err: any, db: { db: (arg0: string) => any; close: () => void; }) {
+        if (err) throw err;
+        const dbo = db.db(DATABASE);
+        dbo.createCollection(collectionName, function (err: any, res: any) {
+            if (err) throw err;
+            console.log("Collection created!");
+            collections.push(collectionName);
+            db.close();
+        });
+    });
+}
+
+function insertRecord(record: any) {
+    mongoClient.connect(MONGO_URL, function (err: any, db: { db: (arg0: string) => any; close: () => void; }) {
+        if (err) throw err;
+        const dbo = db.db(DATABASE);
+        dbo.collection(collections[0]).insertOne(record, function (err: any, res: any) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+}
+
+function insertRecords(records: any) {
+
+    mongoClient.connect(MONGO_URL, function (err: any, db: { db: (arg0: string) => any; close: () => void; }) {
+        if (err) throw err;
+        const dbo = db.db(DATABASE);
+        dbo.collection(collections[0]).insertMany(records, function (err: any, res: { insertedCount: string; }) {
+            if (err) throw err;
+            console.log("Number of documents inserted: " + res.insertedCount);
+            db.close();
+        });
+    });
+}
