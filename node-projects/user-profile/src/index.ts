75f35b2d4d917ac99ce00b5b627ec1da9d8a4a54
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-12-13 23:01:08 +0530

    Added Express

--- node-projects/user-profile/src/index.ts
+++ node-projects/user-profile/src/index.ts
@@ -1,9 +1,13 @@
-let mango = require("mongodb");
+import mango from "mongodb";
+import express from "express";
+
+const app = express();
 const mongoClient = mango.MongoClient;
 
+const PORT = 3000;
 const MONGO_URL: string = "mongodb://localhost:27017/";
-let DATABASE: string = "userprofile";
 const collections: string[] = ["customers"];
+let DATABASE: string = "userprofile";
 
 function createDatabase(databaseName: string) {
     const URL = `${MONGO_URL}/${databaseName}`;
@@ -72,4 +76,41 @@ function getRecords(query: any): any[] {
     return data;
 }
 
-getRecords({ address: /^H/ });
+function deleteRecords(query: any): any {
+    mongoClient.connect(MONGO_URL, function (err, db) {
+        if (err) throw err;
+        const dbo = db.db(DATABASE);
+        dbo.collection(collections[0]).deleteMany(query, function (err, obj) {
+            if (err) throw err;
+            console.log(obj.result.n + " document(s) deleted");
+            db.close();
+        });
+    });
+}
+
+function updateRecords(currentRecord: any, newRecord: any): any {
+    mongoClient.connect(MONGO_URL, function (err, db) {
+        if (err) throw err;
+        const dbo = db.db(DATABASE);
+
+        dbo.collection(collections[0]).updateMany(currentRecord, newRecord, function (err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
+            db.close();
+        });
+    });
+}
+
+// const myquery = { address: "Central st 954" };
+// const newvalues = { $set: { address: "Central Park 954" } };
+// updateRecords(myquery, newvalues);
+
+// deleteRecords({ address: /1$/ });
+
+app.get("/", (request, response) => {
+    response.send(`App Powered by Express ${request.path}`);
+});
+
+app.listen(PORT, () => {
+    console.log("Server started");
+});
