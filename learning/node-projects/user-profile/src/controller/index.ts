b84fc8118258bcb75a04d52ac39cd64930ba9c7f
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-12-15 22:45:13 +0530

    Seperated MongoDB controller code with server related code

---
+++ learning/node-projects/user-profile/src/controller/index.ts
@@ -0,0 +1,99 @@
+import mango from "mongodb";
+
+const mongoClient = mango.MongoClient;
+
+const MONGO_URL: string = "mongodb://localhost:27017/";
+const collections: string[] = ["customers"];
+let DATABASE: string = "userprofile";
+
+export function createDatabase(databaseName: string) {
+    const URL = `${MONGO_URL}/${databaseName}`;
+    mongoClient.connect(URL, (error: any, db: { close: () => void; }) => {
+        if (error) {
+            console.error(error);
+            throw error;
+        }
+        console.log("DB created");
+        DATABASE = databaseName;
+        db.close();
+    });
+}
+
+export function createCollection(collectionName: string) {
+    mongoClient.connect(MONGO_URL, function (err: any, db: { db: (arg0: string) => any; close: () => void; }) {
+        if (err) throw err;
+        const dbo = db.db(DATABASE);
+        dbo.createCollection(collectionName, function (err: any, res: any) {
+            if (err) throw err;
+            console.log("Collection created!");
+            collections.push(collectionName);
+            db.close();
+        });
+    });
+}
+
+export function insertRecord(record: any) {
+    mongoClient.connect(MONGO_URL, function (err: any, db: { db: (arg0: string) => any; close: () => void; }) {
+        if (err) throw err;
+        const dbo = db.db(DATABASE);
+        dbo.collection(collections[0]).insertOne(record, function (err: any, res: any) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+}
+
+export function insertRecords(records: any) {
+
+    mongoClient.connect(MONGO_URL, function (err: any, db: { db: (arg0: string) => any; close: () => void; }) {
+        if (err) throw err;
+        const dbo = db.db(DATABASE);
+        dbo.collection(collections[0]).insertMany(records, function (err: any, res: { insertedCount: string; }) {
+            if (err) throw err;
+            console.log("Number of documents inserted: " + res.insertedCount);
+            db.close();
+        });
+    });
+}
+
+export async function getRecords(query: any): Promise<any> {
+    let data = [];
+
+    return new Promise(async (resolve) => {
+        mongoClient.connect(MONGO_URL, async function (err, db) {
+            if (err) throw err;
+            await db.db(DATABASE).collection(collections[0]).find(query).toArray(function (err, result) {
+                if (err) throw err;
+                db.close();
+                data = result;
+                resolve(data);
+            });
+        });
+    });
+}
+
+export function deleteRecords(query: any): any {
+    mongoClient.connect(MONGO_URL, function (err, db) {
+        if (err) throw err;
+        const dbo = db.db(DATABASE);
+        dbo.collection(collections[0]).deleteMany(query, function (err, obj) {
+            if (err) throw err;
+            console.log(obj.result.n + " document(s) deleted");
+            db.close();
+        });
+    });
+}
+
+export function updateRecords(currentRecord: any, newRecord: any): any {
+    mongoClient.connect(MONGO_URL, function (err, db) {
+        if (err) throw err;
+        const dbo = db.db(DATABASE);
+
+        dbo.collection(collections[0]).updateMany(currentRecord, newRecord, function (err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
+            db.close();
+        });
+    });
+}
