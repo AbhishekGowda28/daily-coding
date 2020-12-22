03fde3fbe190824fd6a5a858123815568ed404d8
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-12-22 22:24:30 +0530

    Updated unit test

--- learning/node-projects/user-profile/src/controller/test/db.test.ts
+++ learning/node-projects/user-profile/src/controller/test/db.test.ts
@@ -1,8 +1,8 @@
-const expect = require("chai").expect;
-const request = require("supertest");
+import { expect } from "chai";
 import server from "../../index";
-describe("Test Post /user", () => {
-    it("Posting a new user", (done) => {
+const request = require("supertest");
+describe("Testing /user", () => {
+    it("Adding a new user", (done) => {
         request(server).
             post("/user").
             send({
@@ -13,6 +13,32 @@ describe("Test Post /user", () => {
                 const body = response.body;
                 expect(body).to.true;
                 done();
+            }).catch(err => {
+                done(err);
             });
+    });
+    it("Get list of all users", (done) => {
+        request(server).get("/user").then((response) => {
+            const list = response.body;
+            expect(list).not.null;
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
+    it("Update user", (done) => {
+        request(server).put("/user").send({ _id: "2", name: "updated User Name" }).then(response => {
+            const result = response.body;
+            expect(result).to.true;
+            done();
+        }).catch(err => done(err));
+    });
+
+    it("Delete Existing User", (done) => {
+        request(server).delete("/user").send({_id:"2"}).then(response => {
+            const result = response.body;
+            expect(result).to.true;
+            done();
+        }).catch(err => done(err));
     });
 });
