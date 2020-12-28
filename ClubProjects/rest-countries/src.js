772721c4bccb831c57dbb2214ad97c6985d7d57b
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-12-28 23:00:02 +0530

    API rest countries

---
+++ ClubProjects/rest-countries/src.js
@@ -0,0 +1,24 @@
+const Key = "97bca7c621msh8c0be05f0b9cc41p1e484djsn560ff45e3999";
+const host = "restcountries-v1.p.rapidapi.com";
+const BaseURL = "https://restcountries-v1.p.rapidapi.com"
+const endPoints = {
+    all: "all"
+};
+fetch(`${BaseURL}/${endPoints.all}`, {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": Key,
+        "x-rapidapi-host": host
+    }
+})
+.then(response => {
+    console.log(response);
+    response.json().then(result => {
+        const counties = document.getElementById("counties");
+        const countryName = result.map(country => country.name)
+        counties.innerText = JSON.stringify(countryName);
+    })
+    })
+    .catch(err => {
+        console.error(err);
+    });
