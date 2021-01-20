d905bb8ea19f23d39356c97c94c86e78f534b6d0
Abhishek Gowda <abhi.gowda28@gmail.com>
2021-01-20 22:18:15 +0530

    Google login setup

--- music/src/components/App.tsx
+++ music/src/components/App.tsx
@@ -1,11 +1,29 @@
+import { GoogleLogin } from 'react-google-login';
 import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <button>Login with Google</button>
+      <GoogleWithLogin />
     </div>
   );
 }
 
 export default App;
+
+function GoogleWithLogin() {
+  function onLoginSuccess(response: any) {
+    console.log(response);
+  }
+  function onLoginFailure(response: any) {
+    console.log(response);
+  }
+  return (
+    <GoogleLogin
+      clientId="550963546957-rhos917qpok76esjf20qn9d05gbj86gu.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={onLoginSuccess}
+      onFailure={onLoginFailure}
+    />
+  )
+}
--- music/src/components/__test__/App.test.tsx
+++ music/src/components/__test__/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders Login with google text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Login with Google/i);
   expect(linkElement).toBeInTheDocument();
 });
