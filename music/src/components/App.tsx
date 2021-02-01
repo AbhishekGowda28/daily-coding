a9c54fcf6e3e5409d2fad5535e5814f56f4b1d65
Abhishek Gowda <abhi.gowda28@gmail.com>
2021-02-01 22:51:20 +0530

    Removed Sound Cloud Component

--- music/src/components/App.tsx
+++ music/src/components/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import '../styles/App.css';
 import { GoogleLogin } from "./google";
-import { LoginWithSoundCloud } from "./soundCloud";
-
 
 function App() {
   return (
     <div className="App">
       <GoogleLogin />
-      <LoginWithSoundCloud />
     </div>
   );
 }
--- music/src/components/soundCloud/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function LoginWithSoundCloud() {
-    return (
-        <div className="authorize__soundCloud">
-            SoundCloud
-        </div>
-    )
-}
