b8e355d219e95ea92359bc9b7f3d85373f25136c
Abhishek Gowda <abhi.gowda28@gmail.com>
2021-02-04 20:35:52 +0530

    Update from the token

--- music/src/components/App.tsx
+++ music/src/components/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../styles/App.css';
 import { GoogleLogin } from "./google";
 
-const API_KEY = "AIzaSyAZQF38Vrk9IDqoS__wtMIjJC4exSqb-Ao";
+const API_KEY = "###AIzaSyAZQF38Vrk9IDqoS__wtMIjJC4exSqb-Ao###";
 
 function App() {
   return (
