866f1f48176b18729cf7ed0118023112663a9470
Abhishek Gowda <abhi.gowda28@gmail.com>
2021-02-09 21:46:25 +0530

    Updates

--- music/src/components/App.tsx
+++ music/src/components/App.tsx
@@ -5,10 +5,11 @@ import { GoogleLogin } from "./google";
 const API_KEY = "###AIzaSyAZQF38Vrk9IDqoS__wtMIjJC4exSqb-Ao###";
 
 function App() {
+  const items: any[] = [];
   return (
     <div className="App">
       <section id="video">
-
+        {items.map(item => item)}
       </section>
     </div>
   );
