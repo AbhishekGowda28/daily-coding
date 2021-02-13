3e67bdf816dd9609bc5b9ed7de65d5a0927a7726
Abhishek Gowda <abhi.gowda28@gmail.com>
2021-02-13 22:54:39 +0530

    update

--- music/src/components/App.tsx
+++ music/src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { API_KEY } from "../constants/secrets";
-import { youtubeBaseURL } from "../constants/urls";
+import { YouTube } from "../constants/urls";
 import { get } from "../services/agent";
 import '../styles/App.css';
 
@@ -8,8 +8,7 @@ function App() {
   const items: any[] = [];
   const key = API_KEY.substr(3, 39);
   console.log(key, API_KEY);
-  // get(`${youtubeBaseURL}?key=${key}`).then(response => { console.log(response) });
-  get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM}`).then(response => { console.log(response) });
+  get(`${YouTube.search}part=snippet&q=INNA&type=playlist?key=AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM}`).then(response => { console.log(response) });
   return (
     <div className="App">
       <section id="video">
--- music/src/constants/urls.ts
+++ music/src/constants/urls.ts
@@ -1,2 +1,7 @@
-export const youtubeBaseURL = "https://www.googleapis.com/youtube/v3/search";
-export const YoutubeSearh = "https://www.googleapis.com/youtube/v3/search";
+export const YouTube = {
+    list: "https://www.googleapis.com/youtube/v3/search",
+    search: "https://www.googleapis.com/youtube/v3/search",
+    playlist: "https://www.googleapis.com/youtube/v3/playlists",
+    item: "",
+    playListItems: "https://www.googleapis.com/youtube/v3/playlistItems"
+};
