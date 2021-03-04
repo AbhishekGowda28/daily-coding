a0afa7b4a672d8d2d2df2ce20ee85ff31ccd2c5e
Abhishek Gowda <abhi.gowda28@gmail.com>
2021-03-04 22:29:10 +0530

    Refactored Button component

--- music/src/components/common/Button.tsx
+++ music/src/components/common/Button.tsx
@@ -1,16 +1,12 @@
-import React from "react";
-
 interface ButtonInterface {
     text: string;
     onClick: () => void;
 }
 
-class Button extends React.Component<ButtonInterface> {
-    render() {
-        return (
-            <button onClick={this.props.onClick}>{this.props.text}</button>
-        )
-    }
+function Button(props: ButtonInterface) {
+    return (
+        <button onClick={props.onClick}>{props.text}</button>
+    )
 }
 
 export default Button;
